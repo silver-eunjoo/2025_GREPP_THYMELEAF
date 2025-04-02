a133d4e3c07c2ab43431e37b2003824adcfbad62
silver-eunjoo <leeeun5423@naver.com>
2025-04-02 11:29:31 +0900

    타임리프 : th:if, th:unless with js 문법

--- src/main/java/io/silver/greppthymeleaf/controller/TemplateController2.java
+++ src/main/java/io/silver/greppthymeleaf/controller/TemplateController2.java
@@ -1,10 +1,26 @@
 package io.silver.greppthymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/page")
 public class TemplateController2 {
 
+    @GetMapping("/1")
+    public String syntaxPage1(String username, Model model) {
+        //localhost:8080/page/1
+        model.addAttribute("username", username);
+        return "/syntax/page1";
+    }
 
+    @GetMapping("/2")
+    public String syntaxPage2() {
 
+        
+
+        return "/syntax/page2";
+    }
 }
