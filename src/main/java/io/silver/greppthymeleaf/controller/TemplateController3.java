7aaaf1dd0e5ed27cf78a4b95ab4544e2ef73c6a8
silver-eunjoo <leeeun5423@naver.com>
2025-04-02 15:27:20 +0900

    타임리프 : 레이아웃, 프레그먼트

--- src/main/java/io/silver/greppthymeleaf/controller/TemplateController3.java
+++ src/main/java/io/silver/greppthymeleaf/controller/TemplateController3.java
@@ -1,6 +1,8 @@
 package io.silver.greppthymeleaf.controller;
 
+import io.silver.greppthymeleaf.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,5 +19,19 @@ public class TemplateController3 {
     @GetMapping("/2")
     public String showLayoutPage2() {
         return "/pages/layout_page_2";
+    }
+
+    @GetMapping("/3")
+    public String showLayoutPage3(Model model) {
+        model.addAttribute("isLogin", true);
+        return "/pages/layout_page_3";
+    }
+
+    @GetMapping("/4")
+    public String showLayoutPage4(Model model) {
+        model.addAttribute("isLogin", true);
+        model.addAttribute("post", new Post(1L, "안녕하세요!", "관리자!", "반갑습니다!"));
+
+        return "/pages/layout_page_4";
     }
 }
