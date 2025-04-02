5054db26c780c268ef0e88c4f270e1a3d33c3298
silver-eunjoo <leeeun5423@naver.com>
2025-04-02 10:28:07 +0900

    index page

---
+++ src/main/java/io/silver/greppthymeleaf/GreppThymeleafApplication.java
@@ -0,0 +1,13 @@
+package io.silver.greppthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreppThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreppThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/main/java/io/silver/greppthymeleaf/controller/TemplateController1.java
@@ -0,0 +1,16 @@
+package io.silver.greppthymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TemplateController1 {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    
+
+}
